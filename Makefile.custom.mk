d3b22e4a85508da4cee33ea28f71404f93130f67
Erkan Erol <erkan@giantswarm.io>
2022-05-18 13:38:12 +0300

    Add validatingwebhook and use kustomize for templating upstream resources

---
+++ Makefile.custom.mk
@@ -0,0 +1,29 @@
+##@ App
+
+SHELL := /bin/bash
+
+APPLICATION_NAME="csi-external-snapshotter-app"
+
+EXTERNAL_SNAPSHOTTER_VERSION="v5.0.1"
+
+.PHONY: all
+all: fetch-upstream-manifest apply-kustomize-patches delete-generated-helm-charts release-manifests ## Builds the manifests to publish with a release (alias to release-manifests)
+
+.PHONY: release-manifests
+release-manifests: 
+	# move files from workdir over to helm directury structure
+	./hack/prepare-helmchart.sh ${APPLICATION_NAME}
+
+.PHONY: fetch-upstream-manifest
+fetch-upstream-manifest: ## fetch upstream manifest from
+	# fetch upstream released manifest yaml
+	./hack/sync-version.sh ${EXTERNAL_SNAPSHOTTER_VERSION}
+
+.PHONY: apply-kustomize-patches
+apply-kustomize-patches: ## apply giantswarm specific patches
+	kubectl kustomize config/kustomize -o config/kustomize/tmp
+
+#.PHONY: delete-generated-helm-charts
+delete-generated-helm-charts: # clean workspace and delete manifests
+	@rm -rvf ./helm/${APPLICATION_NAME}/templates/*.yaml
+	@rm -rvf ./helm/${APPLICATION_NAME}/crds/*.yaml
